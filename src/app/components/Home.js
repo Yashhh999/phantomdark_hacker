2842b5bb037a6fcb9597ef6a248f56fe18c95dfe
Yash Gupta <gupta25yash@gmail.com>
2024-06-03 01:23:31 +0530

    ss

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200" >
       {/* Hero Section */}
-      <section className="h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('https://cdn.discordapp.com/attachments/1242493595479441460/1246520938158100622/2fcd0099-3516-40a6-9938-bf7829158846.jpg?ex=665cb096&is=665b5f16&hm=c02fc5137611caff8f18a205d442c4dec66806851e6b789451ea0c64e9b033d8&')" }}>
+      <section className="h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('https://cdn.discordapp.com/attachments/1242493595479441460/1246520938158100622/2fcd0099-3516-40a6-9938-bf7829158846.jpg?ex=665e0216&is=665cb096&hm=af073fb34d4526beac7b38d42474921214e03fe5d84cee18bf86536bafb6e15f&')" }}>
         <div className="container mx-auto flex flex-col-reverse md:flex-row items-center bg-black bg-opacity-50 p-6 md:p-12 rounded-lg">
           <div className="md:w-1/2 text-center md:text-left mt-6 md:mt-0">
             <h1 className="text-4xl md:text-6xl font-bold text-white">Welcome to Doodle Freaks</h1>
@@ -19,7 +19,7 @@ export default function Home() {
             <a href="#portfolio" className="mt-6 inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300">View our Works</a>
           </div>
           <div className="md:w-1/2 text-center">
-            <img src="https://cdn.discordapp.com/attachments/1242493595479441460/1246523534788792340/dc.jpg?ex=665cb301&is=665b6181&hm=843c4c26708b31979e0fd35b2b260e8de150ed1f67c802eb42f157131a96014e&" alt="Phantom" width={400} height={400} className="rounded-full mx-auto" />
+            <img src="https://cdn.discordapp.com/attachments/1242493595479441460/1246523534788792340/dc.jpg?ex=665e0481&is=665cb301&hm=ad282e83e762e14bde1fe9a36196f4bd6da86d17e66d5022b37f5d943d3b5aa3&" alt="Phantom" width={400} height={400} className="rounded-full mx-auto" />
           </div>
         </div>
       </section>
